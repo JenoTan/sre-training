544366f1b23db06c662980299e5fe70dbd8d5dde
Jeno Tan <jenotan@snsoft.my>
2024-08-29 16:09:54 +0800

    Add files via upload

--- project-terraform-infra/s3-bucket/variables.tf
+++ project-terraform-infra/s3-bucket/variables.tf
@@ -1,17 +1,17 @@
 variable "AWS_PROFILE" {
   description	= "AWS profile name"
   type		= string
-  default	= "<SSO Profile>"
+  default	= "jenotanpg"
 }
 
 variable "AWS_REGION" {
   description	= "AWS region"
   type		= string
-  default	= "<region>"
+  default	= "ap-southeast-1"
 }
 
 variable "TFSTATE_S3_NAME" {
   description	= "Unique name for the s3 bucket for tfstate"
   type		= string
-  default	= "<bucket name>"
+  default	= "bj-bucket"
 }
